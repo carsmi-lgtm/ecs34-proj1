1589879364059033bd233c98117e8eae44f2dae4
Carley Smith <carsmi@ucdavis.edu>
2026-01-11 03:54:18 +0000

    Built first 10 string functions

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -1,11 +1,33 @@
 #include "StringUtils.h"
 #include <cctype>
+#include <cmath>
 
 namespace StringUtils{
 
 std::string Slice(const std::string &str, ssize_t start, ssize_t end) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    if (end == 0){
+        end = Temp.length();
+    }
+    if (start < 0){
+        start = start + Temp.length();
+        if (start < 0){
+            start = 0;
+        }
+    }
+    if (end < 0){
+        end = end + Temp.length();
+        if (end < 0){
+            end = 0;
+        }
+    }
+    if (end > Temp.length()){
+        end = Temp.length();
+    }
+    if (start >= end){
+        return "";
+    }
+    return Temp.substr(start, end - start);
 }
 
 std::string Capitalize(const std::string &str) noexcept{
@@ -15,42 +37,94 @@ std::string Capitalize(const std::string &str) noexcept{
 }
 
 std::string Upper(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    int i;
+    for (i = 0; i < Temp.length(); i++){
+        Temp[i] = toupper(Temp[i]);
+    }
+    return Temp;
 }
 
 std::string Lower(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    int i;
+    for (i = 0; i < Temp.length(); i++){
+        Temp[i] = tolower(Temp[i]);
+    }
+    return Temp;
 }
 
 std::string LStrip(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    size_t len = Temp.length();
+    size_t firstChar = Temp.find_first_not_of(" \n\t");
+    if (firstChar == std::string::npos){
+        return "";
+    }
+    return Temp.substr(firstChar, len - firstChar);
 }
 
 std::string RStrip(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    size_t lastChar = Temp.find_last_not_of(" \n\t");
+    if (lastChar == std::string::npos){
+        return "";
+    }
+    return Temp.substr(0,lastChar + 1);
 }
 
 std::string Strip(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    size_t firstChar = Temp.find_first_not_of(" \n\t");
+    if (firstChar == std::string::npos){
+        return "";
+    }
+    size_t lastChar = Temp.find_last_not_of(" \n\t");
+    return Temp.substr(firstChar, lastChar - firstChar + 1);
 }
 
 std::string Center(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    size_t len = Temp.length();
+    size_t fillRight, fillLeft;
+    if (width < len){
+        return Temp;
+    }
+    size_t toFill = width - len;
+    if (toFill % 2 == 0){
+        fillRight = toFill/2;
+        fillLeft = toFill/2;
+    }
+    else {
+        fillLeft = toFill/2;
+        fillRight = toFill - fillLeft;
+
+    }
+    Temp.insert(len, fillRight, fill);
+    Temp.insert(0, fillLeft, fill);
+    return Temp;
 }
 
 std::string LJust(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
-    return "";
+    std::string Temp = str;
+    size_t len = Temp.length();
+    size_t toFill = width - len;
+    if (width < len){
+        return Temp;
+    }
+    Temp.insert(len, toFill, fill);
+    return Temp;
 }
 
 std::string RJust(const std::string &str, int width, char fill) noexcept{
-    // Replace code here
+    std::string Temp = str;
+    size_t len = Temp.length();
+    size_t toFill = width - len;
+    if (width < len){
+        return Temp;
+    }
+    Temp.insert(0, toFill, fill);
+    return Temp;
     return "";
 }
 
